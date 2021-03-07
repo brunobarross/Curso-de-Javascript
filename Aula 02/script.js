e1f1fa06d3082a1e0c558f67948188bb328c8340
Bruno Barros <62890135+brunobarross@users.noreply.github.com>
2021-03-07 16:31:28 -0300

    Add files via upload

---
+++ Aula 02/script.js
@@ -0,0 +1,18 @@
+var n1 = Number(window.prompt('Digite um número: ')); // converte a string em numero
+var n2 = Number(window.prompt('Digite outro número: ')); // 
+
+var soma = n1 + n2;
+window.alert(`a soma entre ${n1} e ${n2} é igual a ${soma}`);
+
+
+//window.alert('A soma dos valores é ' + String(soma)); // concatenação
+
+
+//to.String ou String(variavel); 
+
+// (number + number) para adição
+
+//(sstring + string) para concatenação
+
+//template string : `Eu estou aprendendo ${variavel}´
+
